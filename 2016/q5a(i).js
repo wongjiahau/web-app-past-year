40a4f1ac1ba041c802ce021358c87d2401d2db79
Wong Jia Hau <hou32hou@gmail.com>
2018-05-03 18:18:35 +0800

    Update

---
+++ 2016/q5a(i).js
@@ -0,0 +1,1 @@
+localStorage["images"] = JSON.stringify(images);
---
+++ 2016/q5a(ii).js
@@ -0,0 +1,16 @@
+var images = JSON.parse(localStorage["images"]);
+
+var pointer = 0;
+
+function changeImage () {
+    var imageElement = document.getElementById("image");
+    var currentImage = images[pointer];
+    imageElement.src = currentImage.file;
+    imageElement.alt = currentImage.description;
+    pointer++;
+    if(pointer === images.length) {
+        pointer = 0;
+    }
+}
+
+setInterval(changeImage, "5000");
