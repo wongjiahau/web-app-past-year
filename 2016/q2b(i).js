6c338df69f9831be648bb566bceeee0e266c16ea
Wong Jia Hau <hou32hou@gmail.com>
2018-05-03 17:21:30 +0800

    Update

--- 2016/q2b(i).js
+++ 2016/q2b(i).js
@@ -1,6 +1,11 @@
 var persons = [{"name": "hello", "gender": "hello"}];
 var ol = document.createElement("ol");
 ol.type = "1";
+
+// When to use setAttribute? 
+// I also tak tau
+// Refer https://stackoverflow.com/questions/3919291/when-to-use-setattribute-vs-attribute-in-javascript
+
 for (var i = 0; i < persons.length; i++) {
     var li = document.createElement("li");
     var personName = persons[i].name;
