c5df317bd7bfa94b859362c69a05b4b3080a04a5
Wong Jia Hau <hou32hou@gmail.com>
2018-05-03 16:51:26 +0800

    Update

---
+++ 2017/q5c.php
@@ -0,0 +1,26 @@
+<?php
+require("database.php");
+$transaction_id = $_COOKIE["cart"];
+$customer_id    = $_COOKIE["customer_id"];
+$date = date("Y-m-d H:i:s");
+
+$conn->query("
+    INSERT INTO transaction(date, customer_id) VALUES ($date, $customer_id);
+");
+
+$items = json_decode($_COOKIE["items"]);
+foreach($items as $i) {
+    $product_id = $i["id"];
+    $quantity   = $i["quantity"];
+    $unit_price = $i["unit_price"];
+}
+$conn->query("
+    INSERT INTO item(product_id, quantity, unit_price, transaction_id) VALUES (
+        $product_id,
+        $quantity,
+        $unit_price,
+        $transaction_id
+    );
+");
+
+?>
